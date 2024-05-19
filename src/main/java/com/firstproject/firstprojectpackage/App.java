f0098d95bf6ee017713e3091820fd5296924b825
krishna <krishna.majja@gmail.com>
2024-05-19 13:13:27 +0530

    adding sources

---
+++ src/main/java/com/firstproject/firstprojectpackage/App.java
@@ -0,0 +1,13 @@
+package com.firstproject.firstprojectpackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
